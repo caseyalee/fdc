27b4f016adefb7f212b8b831f9f1feebabb395a8
Casey Lee <caseyalee@gmail.com>
2021-02-04 04:09:25 -0600

    updates

--- web/app/themes/fdc/inc/post-types.php
+++ web/app/themes/fdc/inc/post-types.php
@@ -11,33 +11,33 @@ include_once get_template_directory() . '/lib/class-cpt.php';
  * @var CPT
  */
 
-$products = new CPT(
- // Labels
- array(
-     'post_type_name' => 'product',
-     'singular' => 'Product',
-     'plural' => 'Products',
-     'slug' => 'product'
- ),
- // Description
- array(
-     'supports' => array(
-         'title', 'editor', 'thumbnail'
-     ),
-     'menu_icon' => 'dashicons-cart', // https://developer.wordpress.org/resource/dashicons
-     'has_archive' => true,
-     'public' => true,
- )
-);
+// $products = new CPT(
+//  // Labels
+//  array(
+//      'post_type_name' => 'product',
+//      'singular' => 'Product',
+//      'plural' => 'Products',
+//      'slug' => 'product'
+//  ),
+//  // Description
+//  array(
+//      'supports' => array(
+//          'title', 'editor', 'thumbnail'
+//      ),
+//      'menu_icon' => 'dashicons-cart', // https://developer.wordpress.org/resource/dashicons
+//      'has_archive' => true,
+//      'public' => true,
+//  )
+// );
 
-// Products Taxonomy
-$products->register_taxonomy(
-    array(
-       'taxonomy_name' => 'product_category',
-       'singular' => 'Category',
-       'plural' => 'Categories'
-    ),array(
-        'hierarchical' => false,
-        'publicly_queryable' => true,
-    )
-);
+// // Products Taxonomy
+// $products->register_taxonomy(
+//     array(
+//        'taxonomy_name' => 'product_category',
+//        'singular' => 'Category',
+//        'plural' => 'Categories'
+//     ),array(
+//         'hierarchical' => false,
+//         'publicly_queryable' => true,
+//     )
+// );

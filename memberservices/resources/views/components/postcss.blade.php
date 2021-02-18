7be07bac414f90a33cb8f65a18232479064f7561
Casey Lee <caseyalee@gmail.com>
2021-02-18 03:43:07 -0600

    update dynamic classes

---
+++ memberservices/resources/views/components/postcss.blade.php
@@ -0,0 +1,7 @@
+<div class="bg-green-300"></div>
+<div class="bg-green-400"></div>
+<div class="bg-yellow-400"></div>
+<div class="bg-red-400"></div>
+<div class="bg-red-400"></div>
+<div class="bg-red-400"></div>
+<div class="bg-red-400"></div>

d8b619107a3fbd4cb75f0efca84a080391490acf
Casey Lee <caseyalee@gmail.com>
2021-02-18 02:52:02 -0600

    testing

--- memberservices/app/Http/Controllers/Mail/SendEmailController.php
+++ memberservices/app/Http/Controllers/Mail/SendEmailController.php
@@ -2,23 +2,30 @@
 namespace App\Http\Controllers\Mail;
 
 use App\Http\Controllers\Controller;
+use App\Mail\UserSubscribed;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
 class SendEmailController extends Controller
 {
 
-    public function send()
+
+
+    public function send(Request $request)
     {
-        $to_name = 'Casey Lee';
-        $to_email = 'caseyalee@gmail.com';
-        $data = array('name'=>"Casey", "body" => "This is a test mail");
-        Mail::send('mail.mail', $data, function($message) use ($to_name, $to_email) {
-            $message->to($to_email, $to_name)
-            ->subject('Laravel Test Mail');
-            $message->from(env('MAIL_USERNAME'),'FDC Admin');
-        });
+        $user = auth()->user();
+        // Mail::to($request->user())
+        Mail::to('caseyalee@gmail.com')->send(new UserSubscribed($user));
+
+    }
+
+
+
+    public function preview(Request $request)
+    {
+        $button = 'https://faithdrivenconsumer.com/contact/';
+        $user = auth()->user();
+        return new UserSubscribed($user,$button);
     }
 
 }

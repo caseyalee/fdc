c27b4ed0c90a37eef080f293bed7a1fa600778b1
Casey Lee <caseyalee@gmail.com>
2021-02-19 01:12:19 -0600

    Add subscription style properties to $user Model

--- memberservices/app/Models/User.php
+++ memberservices/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Laravel\Cashier\Billable;
+use Creativeorange\Gravatar\Facades\Gravatar;
 
 class User extends Authenticatable
 {
@@ -59,6 +60,78 @@ class User extends Authenticatable
     public function getAccessMemberIdAttribute()
     {
         return 'fdc_user_id'.$this->id;
+    }
+
+    public function getAvatarAttribute()
+    {
+        return Gravatar::get($this->email);
+    }
+
+    public function getSubscriptionStatusLabelAttribute()
+    {
+        $status = '';
+        if ($this->subscribed('default')) {
+            $subscription = $this->subscription('default');
+            $status = $subscription->stripe_status;
+        }
+        return $this->getStatusLabelProperty($status,'text');
+    }
+
+    public function getSubscriptionStatusLabelColorAttribute()
+    {
+        $status = '';
+        if ($this->subscribed('default')) {
+            $subscription = $this->subscription('default');
+            $status = $subscription->stripe_status;
+        }
+        return $this->getStatusLabelProperty($status,'color');
+    }
+
+    public function getStatusLabelProperty($status,$type='color')
+    {
+        $statuses = array(
+            'trialing' => [
+                'color' => 'bg-green-100 text-green-800',
+                'text' => 'Trial'
+            ],
+            'active' => [
+                'color' => 'bg-green-400 text-green-900',
+                'text' => 'Active'
+            ],
+            'incomplete' => [
+                'color' => 'bg-yellow-400 text-yellow-900',
+                'text' => 'Incomplete'
+            ],
+            'incomplete_expired' => [
+                'color' => 'bg-red-600 text-white',
+                'text' => 'Expired'
+            ],
+            'past_due' => [
+                'color' => 'bg-red-600 text-white',
+                'text' => 'Past Due'
+            ],
+            'canceled' => [
+                'color' => 'bg-red-600 text-white',
+                'text' => 'Cancelled'
+            ],
+            'unpaid' => [
+                'color' => 'bg-red-600 text-white',
+                'text' => 'Unpaid'
+            ],
+        );
+
+        if ($type == 'color') {
+            if (isset($statuses[$status])) {
+                return $statuses[$status]['color'];
+            }
+            return 'bg-gray-100 text-gray-500';
+        } elseif ($type == 'text') {
+            if (isset($statuses[$status])) {
+                return $statuses[$status]['text'];
+            }
+            return 'N/A';
+        }
+
     }
 
 

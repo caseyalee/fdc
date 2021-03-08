6653dc0f1998183d3a58636890f329055a885829
Casey Lee <caseyalee@gmail.com>
2021-03-08 13:22:49 -0600

    Add brands post type

--- web/app/themes/fdc/inc/post-types.php
+++ web/app/themes/fdc/inc/post-types.php
@@ -30,14 +30,104 @@ $podcasts = new CPT(
  )
 );
 
-// // Products Taxonomy
-// $products->register_taxonomy(
-//     array(
-//        'taxonomy_name' => 'product_category',
-//        'singular' => 'Category',
-//        'plural' => 'Categories'
-//     ),array(
-//         'hierarchical' => false,
-//         'publicly_queryable' => true,
-//     )
-// );
+$brands = new CPT(
+ // Labels
+ array(
+     'post_type_name' => 'brand',
+     'singular' => 'Brand',
+     'plural' => 'Brands',
+     'slug' => 'brand-review'
+ ),
+ // Description
+ array(
+     'supports' => array('title'),
+     'menu_icon' => get_stylesheet_directory_uri().'/assets/img/cross-icon.svg', // https://developer.wordpress.org/resource/dashicons
+     'has_archive' => true,
+     'public' => true,
+ )
+);
+
+
+// Brands Types Taxonomy
+$brands->register_taxonomy(
+    array(
+       'taxonomy_name' => 'type',
+       'singular' => 'Type',
+       'plural' => 'Types',
+    ),array(
+        'hierarchical' => true,
+        'publicly_queryable' => true,
+    )
+);
+
+
+// Brands Categories Taxonomy
+$brands->register_taxonomy(
+    array(
+       'taxonomy_name' => 'category',
+       'singular' => 'Category',
+       'plural' => 'Categories'
+    ),array(
+        'hierarchical' => true,
+        'publicly_queryable' => true,
+    )
+);
+// Brands Keywords Taxonomy
+$brands->register_taxonomy(
+    array(
+       'taxonomy_name' => 'keyword',
+       'singular' => 'Tag',
+       'plural' => 'Tags'
+    ),array(
+        'hierarchical' => false,
+        'publicly_queryable' => true,
+    )
+);
+
+
+
+
+add_filter('wp_terms_checklist_args', 'htmlandcms_select_one_category');
+function htmlandcms_select_one_category($args) {
+    if (isset($args['taxonomy']) && $args['taxonomy'] == 'type') {
+        $args['walker'] = new Walker_Category_Radios;
+        $args['checked_ontop'] = false;
+    }
+    return $args;
+}
+
+class Walker_Category_Radios extends Walker {
+    var $tree_type = 'category';
+    var $db_fields = array ('parent' => 'parent', 'id' => 'term_id');
+
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent<ul class='children'>\n";
+    }
+
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul>\n";
+    }
+
+    function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
+        extract($args);
+        if ( empty($taxonomy) )
+            $taxonomy = 'category';
+
+        if ( $taxonomy == 'category' )
+            $name = 'post_category';
+        else
+            $name = 'tax_input['.$taxonomy.']';
+
+        /** @var $popular_cats */
+        $class = in_array( $object->term_id, $popular_cats ) ? ' class="popular-category"' : '';
+        /** @var $selected_cats */
+        $output .= "\n<li id='{$taxonomy}-{$object->term_id}'$class>" . '<label class="selectit"><input value="' . $object->term_id . '" type="radio" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $object->term_id . '"' . checked( in_array( $object->term_id, $selected_cats ), TRUE, FALSE ) . disabled( empty( $args['disabled'] ), FALSE, FALSE ) . ' /> ' . esc_html( apply_filters('the_category', $object->name )) . '</label>';
+    }
+
+    function end_el( &$output, $category, $depth = 0, $args = array() ) {
+        $output .= "</li>\n";
+    }
+}
+

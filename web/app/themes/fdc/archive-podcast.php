3106f94d2d852c7047bc5d78e208b76d4ab44162
Casey Lee <caseyalee@gmail.com>
2021-03-08 12:12:57 -0600

    use iframe for podcast

--- web/app/themes/fdc/archive-podcast.php
+++ web/app/themes/fdc/archive-podcast.php
@@ -18,59 +18,9 @@
 
 <div class="spacer lg:py-12"></div>
 
-<div class="lg:pl-72" id="mainbody">
+<div class="lg:pl-72 mt-6" id="mainbody">
     <div class="container max-w-screen-3xl">
-
-        <?php if ( have_posts() ) : ?>
-            <div class="lg:flex equalize" data-equalize-options='{"target":".entry-title"}'>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <div class="lg:w-1/2 lg:px-6">
-                    <?php do_action( 'theme_before_entry' ); ?>
-                    <article id="post-<?php the_ID(); ?>" <?php post_class('prose max-w-7xl prose-lg'); ?>>
-
-                        <header class="entry-header">
-                            <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-                        </header><!-- .entry-header -->
-
-                        <?php
-                        if (has_post_thumbnail( $post )) {
-                            echo get_the_post_thumbnail( $post, 'large',['class'=>'mb-0 w-full h-auto']);
-                        }
-                        ?>
-
-                        <div class="entry-meta text-sm text-gray-600 py-2">
-                            <?php theme_entry_header(); ?>
-                        </div>
-
-                        <div class="entry-content text-base">
-                            <?php the_field('post_excerpt'); ?>
-                            <div class="float-right mt-8">
-                                <?php
-                                    $more_link_text = (get_post_type() == 'podcast') ? 'Listen' : 'Continue Reading';
-                                    $more_link_icon = (get_post_type() == 'podcast') ? 'fad fa-microphone' : 'fad fa-bookmark';
-                                ?>
-                                <a class="button button-sm" href="<?php echo get_permalink(); ?>"><i class="<?php echo $more_link_icon; ?>"></i> <?php echo $more_link_text; ?></a>
-                            </div>
-                        </div><!-- .entry-content -->
-                        <hr>
-
-                    </article><!-- #post-## -->
-                </div>
-
-                <?php do_action( 'theme_after_entry' ); ?>
-
-            <?php endwhile; ?>
-
-            </div> <!-- /.flex -->
-
-            <?php the_posts_navigation(); ?>
-
-        <?php else : ?>
-
-            <?php get_template_part( 'loops/content', 'none' ); ?>
-
-        <?php endif; ?>
-
+        <iframe height="480px" width="100%" frameborder="no" scrolling="no" seamless src="https://player.simplecast.com/94751a5f-9c72-4503-8bd7-abcf24d9f0d9?dark=false&show=true"></iframe>
     </div>
 </div>
 
